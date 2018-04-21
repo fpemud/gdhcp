4866fdd2d6ad7755baeee16c8c1b7b3917df9436
fpemud <fpemud@sina.com>
2018-04-21 15:59:55 +0800

    fix

--- examples/dhcp-server-test.c
+++ examples/dhcp-server-test.c
@@ -1,29 +1,12 @@
-/*
- *
- *  Connection Manager
- *
- *  Copyright (C) 2007-2012  Intel Corporation. All rights reserved.
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 2 as
- *  published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
+/* build with:
+ *   gcc `pkg-config --cflags --libs gdhcp-1.0` dhcp-server-test.c
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include <gdhcp/gdhcp.h>
+#include <gdhcp.h>
 
 static GMainLoop *main_loop;
 
